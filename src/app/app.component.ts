ee3eaa2f0a6e5feeb255214e3d43ea59a3ec1c20
arunbabugsd <arun.sureshbabu@gosmartdining.com>
2024-06-22 13:32:34 +0530

    Initiated nav page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {TopNavComponent} from "./top-nav/top-nav.component";
 import {HeroSectionComponent} from "./hero-section/hero-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopNavComponent, HeroSectionComponent],
+  imports: [RouterOutlet, TopNavComponent, HeroSectionComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ClassesComponent} from "./classes/classes.component";
+import {InstructorsComponent} from "./instructors/instructors.component";
+import {ContactComponent} from "./contact/contact.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'classes', component: ClassesComponent},
+  {path: 'instructors', component: InstructorsComponent},
+  {path: 'contact', component: ContactComponent},
+];
